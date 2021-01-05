0432ecf305fa6f7bd8030fc5ea168e92cd5745e3
Noah Budarf <ncbudarf@gmail.com>
2021-01-05 14:46:08 -0800

    Initial Commit

---
+++ BetWorks/BetWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetWorksApp.swift
+//  BetWorks
+//
+//  Created by Noah budarf on 2021-01-05.
+//
+
+import SwiftUI
+
+@main
+struct BetWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
